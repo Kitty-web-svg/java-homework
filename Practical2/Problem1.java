e0ab5a7dde4cd81b7201a7f32533c5fcba65d2e4
JIAYI LI <j.li11.22@abdn.ac.uk>
2023-10-31 12:05:57 +0000

    p2

---
+++ Practical2/Problem1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Problem1{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int start = input.nextInt();
+        int end = input.nextInt();
+        int sum = 0;
+        if (start > end){
+            System.out.println("Start number shouldn't be bigger than end number!");
+        }
+        else{
+        for (int i = start; i <= end; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+        }
+    }
+}
